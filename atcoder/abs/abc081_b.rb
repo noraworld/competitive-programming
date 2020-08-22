93040901b55ae4be05cb22fb564771860301ecbc
noraworld <mail@noraworld.com>
2020-08-23 08:15:20 +0900

    Done abc081_b

---
+++ atcoder/abs/abc081_b.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+def main
+  integers = readlines.last.split(' ').map(&:to_i)
+  count = half_count(integers, 0)
+  puts count
+end
+
+def half_count(integers, count)
+  if integers.all?(&:even?)
+    half_count(integers.map { |v| v / 2 }, count + 1)
+  else
+    count
+  end
+end
+
+main
